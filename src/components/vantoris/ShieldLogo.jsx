8b5f696de75ba353cda6a161617ce1c1f625c211
Colton Hogan <itsandrewjack@gmail.com>
2026-06-30 01:30:43 +0000

    File changes

--- src/components/vantoris/ShieldLogo.jsx
+++ src/components/vantoris/ShieldLogo.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function ShieldLogo({ size = 40, className = '' }) {
   return (
     <img
-      src="https://media.base44.com/images/public/6a3d85c1632966fefe16f3d4/15d7d05e4_9B847EAE-49B4-4EAD-B7DE-072A83629039.png"
+      src="https://media.base44.com/images/public/6a3d85c1632966fefe16f3d4/90bb62306_image.png"
       alt="Vantoris"
       className={className}
       style={{ width: size, height: size, objectFit: 'contain' }}
